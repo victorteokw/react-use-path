60c8696495ebfc4e2b1a11fae53019017423b5dc
Victor Zhang <yeannylam@gmail.com>
2021-01-26 11:12:23 +0800

    fix again

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,13 @@
-export * from 'lib/index.d.ts'
+interface Path {
+    pathname: string;
+    query: string;
+    hash: string;
+    path: string;
+}
+declare type PathSetter = string | Partial<Path>;
+interface SetPath {
+    (newPath: PathSetter, noRecord: boolean): void;
+    (newPath: PathSetter): void;
+}
+declare const usePath: () => [Path, SetPath, SetPath];
+export default usePath;
